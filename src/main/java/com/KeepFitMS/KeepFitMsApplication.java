cefc77208a9f71625150bbd5f4d7274df8cded31
whatbugyou <1050466416@qq.com>
2019-08-29 22:43:23 +0800

    项目初始化

---
+++ src/main/java/com/KeepFitMS/KeepFitMsApplication.java
@@ -0,0 +1,13 @@
+package com.KeepFitMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepFitMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepFitMsApplication.class, args);
+	}
+
+}
