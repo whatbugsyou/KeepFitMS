53f0cc124600d5dd87ae302b0134ed26015ab676
whatbugyou <1050466416@qq.com>
2019-09-05 21:22:12 +0800

    hzl:添加redis缓存支持，我的redis没有密码

--- src/main/java/com/KeepFitMS/controller/PersonnelController.java
+++ src/main/java/com/KeepFitMS/controller/PersonnelController.java
@@ -5,14 +5,21 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.KeepFitMS.entity.Emp;
 import com.KeepFitMS.service.EmpService;
-
+/**
+ * 人事管理
+ * @author Hzl
+ *
+ */
 @Controller
+@EnableCaching
 public class PersonnelController {
 	@Autowired
 	private EmpService empService;
@@ -28,8 +35,14 @@ public class PersonnelController {
 	public  String addEmp() {
 		return null;
 	}
+	
+	/**
+	 * 获取所有员工信息
+	 * @return
+	 */
 	@GetMapping("/getAllEmp.do")
 	@ResponseBody
+	@Cacheable(cacheNames = "getAllEmp",key = "" ,unless="#result == null")
 	public  HashMap<String, Object> getAllEmp() {
 		List<Emp> empList = empService.selectAllEmp();
 		HashMap<String, Object> result = new HashMap<>();
