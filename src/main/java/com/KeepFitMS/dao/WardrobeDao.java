8d147e3c2be146f60ce2b012381335761099e22e
LWGGGGGG <18570493846@163.com>
2019-09-07 22:17:14 +0800

    业务功能基本完成，剩下事务以及redis的应用

--- src/main/java/com/KeepFitMS/dao/WardrobeDao.java
+++ src/main/java/com/KeepFitMS/dao/WardrobeDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
@@ -13,20 +14,23 @@ import com.KeepFitMS.entity.Wardroberecord;
 @Mapper
 public interface WardrobeDao {
 	
-	@Select("select * from wardrobe")
+	@Select("select * from wardrobe order by wardrobe_name asc")
 	List<Wardrobe> selectAllW();
 	
-	@Select("select * from wardroberocords")
+	@Select("select * from wardroberecords group by wr_status desc,wr_regtime desc")
 	List<Wardroberecord> selectAllWR();
 	
-	@Update("update wardrobe set wardrobe_name=#{wardrobe_name},wardrobe_status=#{wardrobe_status} where wardrobe_id=#{wardrobe_id}")
-	int updateW(Wardrobe wardrobe);
+	@Select("select * from wardroberecords where wr_id = #{wr_id}")
+	Wardroberecord selectWRById(Integer wr_id);
 	
-	@Update("update wardroberocords set cid=#{cid},mname=#{mname},wardrobe_name=#{wardrobe_name},wr_regtime=#{wr_regtime},wr_status=#{wr_status}"
+	@Update("update wardrobe set wardrobe_status=#{wardrobe_status} where wardrobe_name=#{wardrobe_name}")
+	int updateWByWname(@Param("wardrobe_status") Integer wstatus,@Param("wardrobe_name") String wname);
+	
+	@Update("update wardroberecords set cid=#{cid},mname=#{mname},wardrobe_name=#{wardrobe_name},wr_regtime=#{wr_regtime},wr_status=#{wr_status},"
 			+ "wr_starttime=#{wr_starttime},wr_endtime=#{wr_endtime},wr_deposit=#{wr_deposit},wr_desc=#{wr_desc} where wr_id=#{wr_id}")
 	int updateWR(Wardroberecord wardroberecord);
 	
-	@Insert("insert into wardroberocords(cid,mname,wardrobe_name,wr_regtime,wr_status,wr_starttime,wr_endtime,wr_deposit,wr_desc) "
+	@Insert("insert into wardroberecords(cid,mname,wardrobe_name,wr_regtime,wr_status,wr_starttime,wr_endtime,wr_deposit,wr_desc) "
 			+ "values(#{cid},#{mname},#{wardrobe_name},#{wr_regtime},#{wr_status},#{wr_starttime},#{wr_endtime},#{wr_deposit},#{wr_desc})")
 	int insertWR(Wardroberecord wardroberecord);
 	
--- src/main/java/com/KeepFitMS/service/WardrobeService.java
+++ src/main/java/com/KeepFitMS/service/WardrobeService.java
@@ -3,10 +3,19 @@ package com.KeepFitMS.service;
 import java.util.List;
 
 import com.KeepFitMS.entity.Wardrobe;
+import com.KeepFitMS.entity.Wardroberecord;
 
 
 public interface WardrobeService {
 	
 	List<Wardrobe> selectAllW();
+	
+	List<Wardroberecord> selectAllWR();
+	
+	boolean rentW(String cid,String mname,String wardrobe_name,String wr_deposit,String wr_desc);
+
+	boolean cancelW(String wr_id);
+	
+	boolean setWStatus(String wardrobe_name,String wardrobe_status);
 
 }
