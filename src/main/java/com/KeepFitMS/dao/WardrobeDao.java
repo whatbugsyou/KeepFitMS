e5bb9ed83292ad1bef95f70946e36d16f041c3f5
LWGGGGGG <18570493846@163.com>
2019-09-07 23:05:07 +0800

    基本完成了衣柜管理的所有功能，还剩下事务、redis、以及并发问题的处理

--- src/main/java/com/KeepFitMS/dao/WardrobeDao.java
+++ src/main/java/com/KeepFitMS/dao/WardrobeDao.java
@@ -10,32 +10,61 @@ import org.apache.ibatis.annotations.Update;
 
 import com.KeepFitMS.entity.Wardrobe;
 import com.KeepFitMS.entity.Wardroberecord;
-
+/**
+ * @衣柜管理接口
+ * @author Lwg
+ *
+ */
 @Mapper
 public interface WardrobeDao {
-	
+	/**
+	 * @apiNote 查询所有衣柜
+	 * @param 无
+	 * @return 衣柜集合
+	 */
 	@Select("select * from wardrobe order by wardrobe_name asc")
 	List<Wardrobe> selectAllW();
 	
+	/**
+	 * @apiNote 查询所有衣柜出租记录
+	 * @param 无
+	 * @return 衣柜出租记录集合
+	 */
 	@Select("select * from wardroberecords group by wr_status desc,wr_regtime desc")
 	List<Wardroberecord> selectAllWR();
 	
+	/**
+	 * @apiNote 根据衣柜出租记录的id查询衣柜出租记录
+	 * @param Integer wr_id
+	 * @return 衣柜出租记录
+	 */
 	@Select("select * from wardroberecords where wr_id = #{wr_id}")
 	Wardroberecord selectWRById(Integer wr_id);
 	
+	/**
+	 * @apiNote 根据衣柜名称修改衣柜的状态
+	 * @param Integer wstatus, String wname
+	 * @return int
+	 */
 	@Update("update wardrobe set wardrobe_status=#{wardrobe_status} where wardrobe_name=#{wardrobe_name}")
 	int updateWByWname(@Param("wardrobe_status") Integer wstatus,@Param("wardrobe_name") String wname);
 	
+	/**
+	 * @apiNote 修改衣柜出租记录
+	 * @param Wardroberecord wardroberecord
+	 * @return int
+	 */
 	@Update("update wardroberecords set cid=#{cid},mname=#{mname},wardrobe_name=#{wardrobe_name},wr_regtime=#{wr_regtime},wr_status=#{wr_status},"
 			+ "wr_starttime=#{wr_starttime},wr_endtime=#{wr_endtime},wr_deposit=#{wr_deposit},wr_desc=#{wr_desc} where wr_id=#{wr_id}")
 	int updateWR(Wardroberecord wardroberecord);
 	
+	/**
+	 * @apiNote 插入衣柜出租记录
+	 * @param Wardroberecord wardroberecord
+	 * @return int
+	 */
 	@Insert("insert into wardroberecords(cid,mname,wardrobe_name,wr_regtime,wr_status,wr_starttime,wr_endtime,wr_deposit,wr_desc) "
 			+ "values(#{cid},#{mname},#{wardrobe_name},#{wr_regtime},#{wr_status},#{wr_starttime},#{wr_endtime},#{wr_deposit},#{wr_desc})")
 	int insertWR(Wardroberecord wardroberecord);
-	
-	
-	
-	
 
 }
--- src/main/java/com/KeepFitMS/service/WardrobeService.java
+++ src/main/java/com/KeepFitMS/service/WardrobeService.java
@@ -4,18 +4,46 @@ import java.util.List;
 
 import com.KeepFitMS.entity.Wardrobe;
 import com.KeepFitMS.entity.Wardroberecord;
-
+/**
+ * @衣柜service接口
+ * @author 国国
+ *
+ */
 
 public interface WardrobeService {
-	
+	/**
+	 * @apiNote 查询所有衣柜
+	 * @param 无
+	 * @return 衣柜集合
+	 */
 	List<Wardrobe> selectAllW();
 	
+	/**
+	 * @apiNote 查询所有衣柜出租记录
+	 * @param 无
+	 * @return 衣柜出租记录集合
+	 */
 	List<Wardroberecord> selectAllWR();
 	
+	/**
+	 * @apiNote 租柜
+	 * @param 会员卡号，会员姓名，衣柜名称，衣柜押金，租柜备注
+	 * @return boolean
+	 */
 	boolean rentW(String cid,String mname,String wardrobe_name,String wr_deposit,String wr_desc);
 
+	/**
+	 * @apiNote 退柜
+	 * @param 衣柜出租记录的id
+	 * @return boolean
+	 */
 	boolean cancelW(String wr_id);
 	
+	/**
+	 * @apiNote 设置衣柜故障状态与否
+	 * @param 衣柜名称，衣柜状态
+	 * @return boolean
+	 */
 	boolean setWStatus(String wardrobe_name,String wardrobe_status);
 
 }
