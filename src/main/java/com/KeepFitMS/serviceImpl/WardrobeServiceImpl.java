5805227e2e4cbdd0783db1e3307d478f1a252279
LWGGGGGG <18570493846@163.com>
2019-09-09 10:55:15 +0800

    完善衣柜管理

--- src/main/java/com/KeepFitMS/serviceImpl/WardrobeServiceImpl.java
+++ src/main/java/com/KeepFitMS/serviceImpl/WardrobeServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,20 +22,40 @@ public class WardrobeServiceImpl implements WardrobeService{
 	@Resource
 	private WardrobeDao wardrobeDao;
 	
+	@Resource
+	private RedisTemplate redisTemplate;
+	
 	@Override
 	public List<Wardrobe> selectAllW() {
-		return wardrobeDao.selectAllW();
+		//从redis中获取数据
+		List<Wardrobe> wList = (List<Wardrobe>) redisTemplate.boundValueOps("wList").get();
+		//判断redis中是否存在数据
+		if(wList == null) {//redis中不存在，从数据库中获取
+			wList = wardrobeDao.selectAllW();
+			//将结果存储到redis中
+			redisTemplate.boundValueOps("wList").set(wList);
+			//System.out.println("数据库获得的");
+		}//else则是从redis获得的
+		return wList;
 	}
 
 	@Override
 	public List<Wardroberecord> selectAllWR() {
-		return wardrobeDao.selectAllWR();
+		//从redis中获取数据
+		List<Wardroberecord> wRList = (List<Wardroberecord>) redisTemplate.boundValueOps("wRList").get();
+		//判断redis中是否存在数据
+		if(wRList == null) {//redis中不存在，从数据库中获取
+			wRList = wardrobeDao.selectAllWR();
+			//将结果存储到redis中
+			redisTemplate.boundValueOps("wRList").set(wRList);
+		}//else则是从redis获得的
+		return wRList;
 	}
 
 	@Override
 	@Transactional
-	public boolean rentW(String cid, String mname, String wardrobe_name, String wr_deposit, String wr_desc) throws WardrobeServiceException {
-		//先判断是否已租
+	public synchronized boolean rentW(String cid, String mname, String wardrobe_name, String wr_deposit, String wr_desc) throws WardrobeServiceException {
+		//先判断是否已租,抛出异常
 		if(wardrobeDao.selectWStatusByName(wardrobe_name) == 1) {
 			throw new WardrobeServiceException("这个衣柜已经被人抢先租走了！");
 		}
@@ -56,6 +77,9 @@ public class WardrobeServiceImpl implements WardrobeService{
 		wardroberecord.setWr_desc(wr_desc);
 		//添加衣柜出租记录
 		wardrobeDao.insertWR(wardroberecord);
+		//写入之后清空redis的缓存，达到数据同步
+		redisTemplate.delete("wList");
+		redisTemplate.delete("wRList");
 		return true;
 	}
 
@@ -74,6 +98,9 @@ public class WardrobeServiceImpl implements WardrobeService{
 		wardroberecord.setWr_endtime(wr_endtime);
 		//更新衣柜出租记录
 		wardrobeDao.updateWR(wardroberecord);
+		//写入之后清空redis的缓存，达到数据同步
+		redisTemplate.delete("wList");
+		redisTemplate.delete("wRList");
 		return true;
 	}
 
@@ -81,6 +108,8 @@ public class WardrobeServiceImpl implements WardrobeService{
 	public boolean setWStatus(String wardrobe_name, String wardrobe_status) {
 		Integer w_status = Integer.parseInt(wardrobe_status);
 		wardrobeDao.updateWByWname(w_status, wardrobe_name);
+		//写入之后清空redis的缓存，达到数据同步
+		redisTemplate.delete("wList");
 		return true;
 	}
 
